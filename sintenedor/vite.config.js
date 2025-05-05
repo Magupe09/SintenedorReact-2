0c42175b1f2199eee5857d43f2ff89d8f08dc38d
Mauricio <mgp.94@hotmail.com>
2025-05-05 18:19:01 -0500

    Modificación del path

--- sintenedor/vite.config.js
+++ sintenedor/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://magupe09.github.io/SintenedorReact-2',
+  base:'/SintenedorReact-2/'
 })
