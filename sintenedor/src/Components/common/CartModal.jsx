788ec6709f4729142c04016a1cf5f42cf51f0a95
Mauricio <mgp.94@hotmail.com>
2025-05-22 17:13:52 -0500

    Styles CartModule

--- sintenedor/src/Components/common/CartModal.jsx
+++ sintenedor/src/Components/common/CartModal.jsx
@@ -1,20 +1,21 @@
 import React from 'react'; // Solo necesitas React si no usas Hooks como useState aquí
+import styles from './CartModal.module.css';
 
 const CartModal = ({ carrito, onClose }) => { // <-- ¡CORREGIDO! Recibes las props desestructuradas
   return (
-    <div>
+    <div className={styles['cart-modal']}>
       <h2>Tu Carrito de Pizza</h2>
       <button onClick={onClose}>Cerrar</button>
       {/* Aquí irá la lógica para mostrar los ítems del carrito */}
       {carrito.length === 0 ? (
         <p>Tu carrito está vacío.</p>
       ) : (
-        <ul>
+        <ul className={styles['cart-list']}>
           {carrito.map((item, index) => (
             // Por ahora, solo mostramos el nombre de la pizza.
             // Más adelante haremos el componente CartItem.jsx
-            <li key={index}>
-              {item.pizzaInfo.nombre} - Cantidades: {JSON.stringify(item.selectedQuantities)} - Total: ${item.totalItemPrice.toFixed(2)}
+            <li className={styles['cart-item']} key={index}>
+              {item.pizzaInfo.nombre} - {JSON.stringify(item.selectedQuantities)} - Total: ${item.totalItemPrice.toFixed(2)}
             </li>
           ))}
         </ul>
