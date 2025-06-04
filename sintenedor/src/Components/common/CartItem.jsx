31f4ea832d3621320554a0d7f2eda31ed1fb22bc
Mauricio <mgp.94@hotmail.com>
2025-06-04 17:39:54 -0500

    Cambios en el manejo de las cantidades

--- sintenedor/src/Components/common/CartItem.jsx
+++ sintenedor/src/Components/common/CartItem.jsx
@@ -7,11 +7,16 @@ function CartItem({ item, onRemoveFromCart }) {
             <img src={item.pizzaInfo.imagen} alt={item.pizzaInfo.nombre} />
             <div className={styles['cart-item-details']}>
                 <h3>{item.pizzaInfo.nombre}</h3>
-                
-                {Object.entries(item.selectedQuantities).map(([clave,valor]) => (
-                    <span key={clave}>{clave}: ${valor}</span>
+                {Object.entries(item.selectedQuantities).map(([size, quantity]) => (
+                    <div key={size} className={styles['quantity-control-display']}> {/* Nuevo contenedor para agrupar */}
+                        <span>{size.charAt(0).toUpperCase() + size.slice(1)}: ${quantity}</span>
+                        <div className={styles['item-quantity-buttons']}> {/* Botones para este tamaño */}
+                            <button onClick={() => onDecreaseItemQuantity(item.pizzaInfo.id, size)}>-</button> {/* Nueva prop */}
+                            <button onClick={() => onIncreaseItemQuantity(item.pizzaInfo.id, size)}>+</button> {/* Nueva prop */}
+                        </div>
+                    </div>
+                ))}
 
-            ))}
                 <p>Total: ${item.totalItemPrice.toFixed(2)}</p>
                 <button onClick={() => onRemoveFromCart(item.pizzaInfo.id)}>Eliminar</button>
             </div>
