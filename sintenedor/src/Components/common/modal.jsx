40f8bba489610bebf76ab3223785c68df05cb3f7
Mauricio <mgp.94@hotmail.com>
2025-06-21 04:57:32 -0500

    Correccion estilos modalmobil

--- sintenedor/src/Components/common/modal.jsx
+++ sintenedor/src/Components/common/modal.jsx
@@ -4,39 +4,39 @@ import styles from './Modal.module.css';
 import { useState } from 'react';
 
 
-function Modal({ pizza, onClose,onAddToCart}) {
+function Modal({ pizza, onClose, onAddToCart }) {
   const [controles, setControles] = useState({ personal: 0, mediana: 0, familiar: 0 });
-  const [isAnimating,setIsAnimating] = useState(false)
+  const [isAnimating, setIsAnimating] = useState(false)
   console.log("Objeto pizza que llega al Modal:", pizza);
 
-  function handleAddToCartClick(pizza,controles){
-    
-    
-    const pizzaInfo=pizza;
-    const selectedQuantities={};
-    let totalItemPrice=0;
+  function handleAddToCartClick(pizza, controles) {
 
-    for(const clave in controles){
-      
-      if(controles[clave]>0){
-        selectedQuantities[clave]= controles[clave];
-        totalItemPrice +=controles[clave] * pizza.precios[clave]
+
+    const pizzaInfo = pizza;
+    const selectedQuantities = {};
+    let totalItemPrice = 0;
+
+    for (const clave in controles) {
+
+      if (controles[clave] > 0) {
+        selectedQuantities[clave] = controles[clave];
+        totalItemPrice += controles[clave] * pizza.precios[clave]
       }
     }
-    const item={pizzaInfo,selectedQuantities,totalItemPrice}
+    const item = { pizzaInfo, selectedQuantities, totalItemPrice }
     console.log("DEBUG - Modal.jsx: Item antes de añadir al carrito:", JSON.parse(JSON.stringify(item)));
-    console.log('Item siendo añadido al carrito',item)
-    if(item.totalItemPrice >0){
+    console.log('Item siendo añadido al carrito', item)
+    if (item.totalItemPrice > 0) {
       onAddToCart(item);
       setControles({ personal: 0, mediana: 0, familiar: 0 });
       setIsAnimating(true); // Activa la animación
       setTimeout(() => {
         setIsAnimating(false); // Desactiva la animación después de un tiempo
       }, 300);
-    }else{
-        alert('Debes definir las cantidades. ')
-      }
-    
+    } else {
+      alert('Debes definir las cantidades. ')
+    }
+
 
 
 
@@ -58,7 +58,7 @@ function Modal({ pizza, onClose,onAddToCart}) {
     setControles(prevControles => {
       // console.log("Aumentamos el valor",prevControles[size])
       const currentQty = prevControles[size];
-    
+
       if (currentQty > 0) {
         return {
           ...prevControles,
@@ -86,7 +86,10 @@ function Modal({ pizza, onClose,onAddToCart}) {
 
   return (
     // El overlay oscuro (fondo) - cierra el modal al hacer clic fuera
+
     <div className={styles['modal-overlay']} onClick={onClose}>
+
+      {/* Botón de cerrar - usa una 'x' (&times;) */}
 
       {/* La caja principal del modal - evita que el clic dentro lo cierre */}
       <div className={styles['modal-box']} onClick={(e) => e.stopPropagation()}>
@@ -108,7 +111,7 @@ function Modal({ pizza, onClose,onAddToCart}) {
             {Object.entries(precios).map(([size, price]) => (
               // key es importante para las listas en React; el 'size' (ej. 'personal') es único
               <li key={size} className={styles['price-item']}>
-               
+
                 {size.charAt(0).toUpperCase() + size.slice(1)}: ${price.toFixed(3)}
                 <div className={styles['quantity-controls']}>
                   <button className={styles['buttonControls']} onClick={() => handleDecreaseQuantity(size)}>-</button>
@@ -124,8 +127,6 @@ function Modal({ pizza, onClose,onAddToCart}) {
 
         <div className={styles['modal-body']}>
 
-          {/* Botón de cerrar - usa una 'x' (&times;) */}
-          <button onClick={onClose} className={styles['close-button']}>&times;</button>
           {/* Imagen de la pizza en el modal - usa la prop imagen */}
           {imagen && <img src={imagen} alt={`Pizza ${nombre}`} className={styles['modal-image']} />} {/* Usa una clase para estilizar la imagen en el modal */}
 
@@ -138,7 +139,7 @@ function Modal({ pizza, onClose,onAddToCart}) {
           {/* Puedes añadir un div.modal-footer para agrupar botones si quieres */}
           {/* <div className={styles['modal-footer']}> */}
           <button className={`${styles['add-to-cart-button']} ${isAnimating ? styles['animate-pulse'] : ''}`}
-          onClick={() => handleAddToCartClick(pizza,controles)}>Añadir al Carrito</button>
+            onClick={() => handleAddToCartClick(pizza, controles)}>Añadir al Carrito</button>
           {/* </div> */}
         </div>
 
