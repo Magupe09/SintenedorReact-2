7beb547852b8c825d5e42219c2fb71b738321da5
Mauricio <mgp.94@hotmail.com>
2025-07-28 13:49:41 -0500

    agrego logica para corregir error de id pizza

--- sintenedor/src/Components/menu/PizzaItem.jsx
+++ sintenedor/src/Components/menu/PizzaItem.jsx
@@ -11,7 +11,7 @@ function PizzaItem({ pizza ,onPizzaClick}) {
   // Retornamos la estructura JSX para mostrar la información de la pizza
   return (
     // Añadimos una clase para poder darle estilos más adelante (grid item)
-    <div className={styles['pizza-item']}  onClick={() => onPizzaClick(pizza)}>
+    <div className={styles['pizza-item']}  onClick={() => onPizzaClick(id)}>
       {/* Mostramos la imagen si la propiedad 'imagen' existe */}
       {/* La ruta '/src/assets/...' funciona con Vite en desarrollo */}
       {imagen && <img src={imagen} alt={`Pizza ${nombre}`} className={styles['pizza-item-image']} />}
